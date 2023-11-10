e732528c893261b6af20ca49c47dffd98f7514d1
Aya Saad <ayasa3d5@gmail.com>
2023-11-10 22:48:22 +0200

    Adding Data Access Layer

---
+++ Api.DataAccessLayer/Context/AppliactionContext.cs
@@ -0,0 +1,21 @@
+﻿using Api.DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.DataAccessLayer.Context
+{
+    public class AppliactionContext:DbContext
+    {
+        public AppliactionContext(DbContextOptions<AppliactionContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Doctor> Doctors { get; set; }
+    }
+}
